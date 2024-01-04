7a5a1187eff137db9e558ae8be84538da1d0aa87
Amala Benny <147146190+Amalabenny7@users.noreply.github.com>
2024-01-04 12:43:43 +0530

    complated

---
+++ cricket.php
@@ -0,0 +1,19 @@
+<html>
+<body bgcolor="lightyellow">
+<h4></h4>
+ <?php
+$name=["ROHIT SHARMA","MS DHONI","VIRAT KOHLI","BUMRAH","DINESH","RISHAB"];
+    echo "<h4>Cricket players name</h4>
+    <table border='2px'>
+    <tr><th> sl no.</th>
+    <th>player</th>";
+     
+    for ($i=0;$i<6;$i++)
+    {
+    $sl=$i+1;
+    echo "<tr><th>$sl</th><th>$name[$i]</th></tr>";
+    }
+    echo "</table>"
+    ?>
+    </body>
+    </html>
